c96bc371b57ba6c59044368e45bbb8daab15db26
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-08-16 14:28:47 -0300

    adicionando o favicon no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Portifólio - David Gomes',
+  description: 'Desenvolvedor Fullstack'
 }
 
 export default function RootLayout({
@@ -14,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
